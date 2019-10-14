b9112657491013e79d95eaa4841617287cc752e7
Davide <cerdrifix@users.noreply.github.com>
2019-10-14 18:19:01 +0200

    Moved to TLS connection. Certificates valid for localhost

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
+	"time"
 )
 
 const message = "Hello, world!"
+
+var (
+	ServerAddress      = ":8443"
+	CertificateFile    = "certs/localhost.crt"
+	CertificateKeyFile = "certs/localhost.key"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -18,8 +26,39 @@ func main() {
 		}
 	})
 
-	err := http.ListenAndServe(":8088", mux)
+	srv := NewServer(mux, ServerAddress)
+
+	err := srv.ListenAndServeTLS(CertificateFile, CertificateKeyFile)
 	if err != nil {
 		log.Fatalf("Error during server startup: %v", err)
 	}
+}
+
+func NewServer(mux *http.ServeMux, serverAddress string) *http.Server {
+	tlsConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+		},
+
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+	srv := &http.Server{
+		Addr:         serverAddress,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig:    tlsConfig,
+		Handler:      mux,
+	}
+	return srv
 }
