d389d134b0c6381662d891c1a4411ef378bed346
Davide Ceretto <davide.ceretto@consulenti.cedacri.it>
2019-10-14 18:05:18 +0200

    Added Content-Type and Status headers

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ const message = "Hello, world!"
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte(message))
 		if err != nil {
 			fmt.Printf("Error writing the response: %v", err)
